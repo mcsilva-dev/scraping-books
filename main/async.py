517d5963e313917af09b6302fa510440618899ee
miguel <miguelcamposdasilva@gmail.com>
2024-06-18 09:56:42 -0300

    versão assincrona implementada

--- main/async.py
+++ main/async.py
@@ -1,157 +1,108 @@
-
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+import json
 import pandas as pd
 import re
-import asyncio
-from datetime import datetime
 
-NOW = datetime.now()
+
 
 URL = 'https://novatec.com.br/'
+
+SUBLINKS = []
+
+SUBPAGES = []
 
 BOOKS = {
     'Título': [],
     'Descrição': [],
-    'Gênero': [],
     'ISBN': [],
     'Páginas': [],
-    'Preço': [],
-    'Link': []
+    'Ano': [],
+    'Preço': []
 }
 
-async def set_title(soup):
-    if soup.find('h1') is not None:
-        BOOKS['Título'].append(
-            soup.find('h1').get_text()
-        )
-    else: 
-        BOOKS['Título'].append("")
-    
 
-async def set_description(soup):
+async def set_book(soup):
+    await asyncio.sleep(1)
+    if soup.h1 is not None:
+        BOOKS['Título'].append(soup.h1.text)
+    else:
+        BOOKS['Título'].append('')
     if soup.find('div', {'id':'menos'}) is not None:
-        BOOKS['Descrição'].append(
-            soup.find('div', {'id':'menos'}).text.replace('\n', '').strip().replace('\r', '')
-        )
+        BOOKS['Descrição'].append(soup.find('div', {'id':'menos'}).text.replace('\n', '').strip().replace('\r',''))
     else:
-        BOOKS['Descrição'].append("")
-
-
-async def set_isbn(soup):
+        BOOKS['Descrição'].append('')
     if soup.find('span', {'itemprop':'isbn'}) is not None:
-        BOOKS['ISBN'].append(
-            soup.find('span', {'itemprop':'isbn'}).get_text()
-        )
+        BOOKS['ISBN'].append(soup.find('span', {'itemprop':'isbn'}).text)
     else:
-        BOOKS['ISBN'].append("")
+        BOOKS['ISBN'].append('')
+    if soup.find('span', {'class': 'style7'}) is not None:
+        BOOKS['Páginas'].append(soup.find('span', {'class': 'style7'}).text.replace('\r\n', '').split()[-1])
+    else:
+        BOOKS['Páginas'].append('')
+    if soup.find('span', {'class': 'style7'}) is not None:
+        BOOKS['Ano'].append(soup.find('span', {'class': 'style7'}).text.replace('\r\n', '').split()[-3])
+    else:
+        BOOKS['Ano'].append('')
+    if soup.find('span', {'itemprop': 'price'}) is not None:
+        BOOKS['Preço'].append(soup.find('span', {'itemprop': 'price'}).text.replace('\r\n', '').split()[-1])
+    else:
+        BOOKS['Preço'].append('')
+
+
+async def get_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
+
         
+async def get_bsobject(url):
+    html = await get_html(url)
+    return BeautifulSoup(html, 'html.parser')
 
 
-async def set_pages(soup):
-    if soup.find('span', {'class':'style7'}) is not None:
-        BOOKS['Páginas'].append(
-            soup.find('span', {'class':'style7'}).get_text().replace('\r', '').replace('\t','').strip().split()[-1]
-        )
-    else:
-        BOOKS['Páginas'].append("")
-    
-
-async def set_price(soup):
-    if  soup.find('span', {'itemprop':'price'}) is not None:
-        BOOKS['Preço'].append(
-            soup.find('span', {'itemprop':'price'}).get_text()
-        )
-    else:
-        BOOKS['Preço'].append("")
+async def get_gendersmenu(soup):
+    return [categoria.a.text for categoria in soup.find_all('td', {'class':'menu_lateral'})]
 
 
-async def set_gender(genders, index):
-    BOOKS['Gênero'].append(genders['genero'][index])
-    
-
-async def set_link(link):
-    BOOKS['Link'].append(link)
+async def get_menulinks(soup):
+    return [categoria.a['href'] for categoria in soup.find_all('td', {'class':'menu_lateral'})]
 
 
-async def get_books(page, url, index, genders):
-    event = asyncio.get_event_loop()
-    urls = await event.create_task(get_urls(page, event))
-    for link in urls:
-        link = url + link
-        soup = await event.create_task(get_html(link, event))
-        event.create_task(set_title(soup))
-        event.create_task(set_description(soup))
-        event.create_task(set_isbn(soup))
-        event.create_task(set_pages(soup))
-        event.create_task(set_price(soup))
-        event.create_task(set_gender(genders, index))
-        event.create_task(set_link(link))
-        
-
-async def get_urls(url, event):
-    soup = await event.create_task(get_html(url, event))
-    urls = []
-    [urls.append(x["href"]) for x in soup.find_all("a", href=re.compile('^(livros/).*$')) if x["href"] not in urls]
-    return urls
-    
-
-async def get_pages(soup):
-    return [x['href'] for x in soup.find_all('a', href = re.compile('^(lista\\.php\\?)*.*$'))[:-1]]
+async def get_sublinks(soup):
+    SUBLINKS.extend([link['href'] for link in soup.find_all('a', href=re.compile(r'^(livros\/).*$')) if link['href'] not in SUBLINKS])
 
 
-async def get_gender(soup):
-    return [x.a.get_text() for x in soup.find_all('td',{'class':'menu_lateral'})]
-
-
-async def get_links(soup):
-    return [x.a['href'] for x in soup.find_all('td',{'class':'menu_lateral'})]
-    
-    
-async def get_gendersdict(soup, event):
-    genderdict = {
-        'genero': await get_gender(soup),
-        'link': await get_links(soup)
-    }
-    return genderdict
-
-async def get_soupobject(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
-
-
-async def get_html(url, event):
-    html = urlopen(URL)
-    return await event.create_task(get_soupobject(html))  
+async def get_subpages(soup):
+    SUBPAGES.extend([pages['href'] for pages in soup.find_all('a', href=re.compile(r'^(lista\.php\?id).*$'))[:-1] if pages['href'] not in SUBPAGES])
 
 
 async def main():
-    # Capturando o event loop
     event = asyncio.get_event_loop()
-    # Recebendo o objeto BeautifulSoup da primeira pagina do site.
-    soup = await event.create_task(get_html(URL, event))
-    # Capturando os links e nomes das categorias
-    genders = await get_gendersdict(soup, event)
-    # Após obtido link de todas as categorias vamos entrar em cada uma delas e capturar todos os livros
-    for index, link in enumerate(genders["link"]):
-        soup, pages = await asyncio.gather(
-            event.create_task(get_html(link, event)),
-            event.create_task(get_pages(soup))
-        )
-        if pages is not None and len(pages) > 0:
-            new_url = URL + link
-            print(f"acessando: {new_url}")
-            await asyncio.gather(get_books(soup, new_url, index, genders))
-        else:
-            print(f"acessando: {link}")
-            await asyncio.gather(get_books(soup, new_url, index, genders))
+    soup = await get_bsobject(URL)
+    links = await event.create_task(get_menulinks(soup))
+    sublinks_soups = await asyncio.gather(*[get_bsobject(link) for link in links])
+    await asyncio.gather(*[get_subpages(soup) for soup in sublinks_soups])
+    subpages_soups = await asyncio.gather(*[get_bsobject(URL + page) for page in SUBPAGES])
+    await asyncio.gather(*[get_sublinks(soup) for soup in subpages_soups])
+    total = len(SUBLINKS)
+    count = 0
+    books_soups = []
+    # controlando o numero de requisições para nao causar erro 504.
+    while count <= (total - 100):
+        books_soups.extend(await asyncio.gather(*[get_bsobject(URL + sublink) for sublink in SUBLINKS[count:count+100]]))
+        count += 100
+    books_soups.extend(await asyncio.gather(*[get_bsobject(URL + sublink) for sublink in SUBLINKS[count:]]))
+    await asyncio.gather(*[set_book(soup) for soup in books_soups])
 
-
-asyncio.run(main())
-
-
-df = pd.DataFrame(BOOKS)
-df.sort_values(by='Título')
-df.drop_duplicates()
-df.to_excel('books.xlsx', index=False)
-print(datetime.now()-NOW)
+if __name__ == '__main__':
+    asyncio.run(main())
+    try:
+        df = pd.DataFrame(BOOKS)
+        df.sort_values(by='Título')
+        df.drop_duplicates()
+        df.to_excel('books.xlsx', index=False)
+    except Exception as e:
+        with open('books.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(BOOKS, ensure_ascii=False))
